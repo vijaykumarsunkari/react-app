58ccb8379fd22042cae06596201e427125b4c514
vijaykumarsunkari <62521720+vijaykumarsunkari@users.noreply.github.com>
2023-02-03 00:42:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default function App(props) {
   const [userId, getID] = useState("");
   const [button,updateButton]=useState("Update");
   const url =
-    "http://localhost:8080/students";
+    "http://localhost:9090/students";
   
   useEffect(() => {
     getUsers();
